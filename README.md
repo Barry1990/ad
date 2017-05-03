9144ab23c776fa3ad686e4743cb2546564c60826
薛念祥 <xuenianxiang@gmail.com>
2017-05-03 14:11:45 +0800

    alipay

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## Ubuntu下操作
+
 ### 安装jdk1.8
 
     $sudo apt-get install python-software-properties
@@ -15,6 +17,23 @@
 ### 安装maven
 
     $sudo apt-get install maven
+    
+    
+### 下载 alipay-sdk    
+
+    wget http://12832846.d.yyupload.com/down/12832846/alipay-sdk-java20170411150054.jar
+
+### 将 alipay-sdk 添加到本地mvn库中
+
+    mvn install:install-file -DgroupId=com.alipay -DartifactId=sdk-java -Dversion=20170411150054 -Dpackaging=jar -Dfile=alipay-sdk-java20170411150054.jar
+
+### pom中添加alipay-sdk
+
+    <dependency>
+         <groupId>com.alipay</groupId>
+         <artifactId>sdk-java</artifactId>
+         <version>20170411150054</version>
+    </dependency>
 
 ### 执行
 
--- src/main/java/com/gxq/service/OrderService.java
+++ src/main/java/com/gxq/service/OrderService.java
@@ -1,5 +1,9 @@
 package com.gxq.service;
 
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayFundTransToaccountTransferRequest;
+import com.alipay.api.response.AlipayFundTransToaccountTransferResponse;
 import com.gxq.mapper.OrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,12 +31,34 @@ public class OrderService {
 
         String serverDate = df.format(new Date());
 
-        return orderMapper.insertOrderStatus("2017-04-03");
+        return orderMapper.insertOrderStatus(serverDate);
     }
 
-//    public void pay(){
-//        AlipayClient alipayClient = new DefaultAlipayClient("https://openapi.alipay.com/gateway.do","app_id","your private_key","json","GBK","alipay_public_key","RSA2");
-//
-//    }
+    public void pay(){
+
+        AlipayClient alipayClient = new DefaultAlipayClient("https://openapi.alipay.com/gateway.do","app_id","your private_key","json","GBK","alipay_public_key","RSA2");
+        AlipayFundTransToaccountTransferRequest request = new AlipayFundTransToaccountTransferRequest();
+        request.setBizContent("{" +
+                "    \"out_biz_no\":\"3142321423432\"," +
+                "    \"payee_type\":\"ALIPAY_LOGONID\"," +
+                "    \"payee_account\":\"abc@sina.com\"," +
+                "    \"amount\":\"12.23\"," +
+                "    \"payer_show_name\":\"上海交通卡退款\"," +
+                "    \"payee_real_name\":\"张三\"," +
+                "    \"remark\":\"转账备注\"," +
+                "  }");
+
+        try {
+            AlipayFundTransToaccountTransferResponse response = alipayClient.execute(request);
+            if(response.isSuccess()){
+                System.out.println("调用成功");
+            } else {
+                System.out.println("调用失败");
+            }
+        }catch (Exception e){
+            System.out.println("调用失败");
+        }
+
+    }
 
 }
